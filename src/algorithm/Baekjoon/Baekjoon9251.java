74f55f960a5e356bc13c4b847f7f6e56517f75d3
Jeongjun <hsk2454@naver.com>
2021-03-17 16:46:49 +0900

    백준9251 코드 업데이트

--- src/algorithm/Baekjoon/Baekjoon9251.java
+++ src/algorithm/Baekjoon/Baekjoon9251.java
@@ -11,15 +11,15 @@ public class Baekjoon9251 {
         str1 = br.readLine();
         str2 = br.readLine();
 
-        int[][] dp = new int[str1.length()+1][str2.length()+1];
+        int[][] dp = new int[2][str2.length()+1];   //2행만 사용함
         int max = 0;
-        for (int i = 1; i < dp.length; i++) {
-            for (int j = 1; j < dp[0].length; j++) {
-                if (str1.charAt(i-1) == str2.charAt(j-1))
-                    dp[i][j] = dp[i-1][j-1]+1;
-                else
-                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
-                max = Math.max(max, dp[i][j]);
+        for (int i = 1, idx = 0; i < str1.length() + 1; i++, idx = ++idx % 2) {
+            for (int j = 1; j < str2.length() + 1; j++) {
+                if (str1.charAt(i-1) == str2.charAt(j-1))   //문자가 같으면
+                    dp[idx][j] = dp[(idx+1)%2][j-1]+1;  //대각선 왼쪽 윗칸값에 1을 더함
+                else    //문자가 다르면
+                    dp[idx][j] = Math.max(dp[(idx+1)%2][j],dp[idx][j-1]); //왼쪽과 위쪽값중에 큰 값
+                max = Math.max(max, dp[idx][j]);
             }
         }
 
