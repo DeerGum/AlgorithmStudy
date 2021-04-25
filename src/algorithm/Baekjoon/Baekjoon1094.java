2e259185af8c96e73289d54fb97a2035ca842ff4
Jeongjun <hsk2454@naver.com>
2021-04-25 15:18:40 +0900

    Auto stash before merge of "master" and "origin/master"

---
+++ src/algorithm/Baekjoon/Baekjoon1094.java
@@ -0,0 +1,41 @@
+package algorithm.Baekjoon;
+
+import java.io.*;
+
+public class Baekjoon1094 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+    }
+}
+
+/*
+64 -> 24
+32 32
+32
+16 16
+16 8 8
+16 8
+16 4 4
+16 4 2 2
+16 4 2 1 1
+16 4 2 1
+
+1000000
+64
+0100000 32
+0100000 32
+0010000 16
+0010000 16
+0001000 8
+0001000 8
+
+
+지민이가 가지고 있는 막대의 길이를 모두 더한다. 처음에는 64cm 막대 하나만 가지고 있다. 이때, 합이 X보다 크다면, 아래와 같은 과정을 반복한다.
+가지고 있는 막대 중 길이가 가장 짧은 것을 절반으로 자른다.
+만약, 위에서 자른 막대의 절반 중 하나를 버리고 남아있는 막대의 길이의 합이 X보다 크거나 같다면, 위에서 자른 막대의 절반 중 하나를 버린다.
+이제, 남아있는 모든 막대를 풀로 붙여서 Xcm를 만든다.
+ */
