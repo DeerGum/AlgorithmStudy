a36e2110d995879fc41fdbbf759c4b7a86f23f9d
jeongjun Hwang <hsk2454@naver.com>
2021-04-27 19:22:42 +0900

    Auto stash before merge of "master" and "origin/master"

--- src/codingTest/programmers/month_4/Program_3.java
+++ src/codingTest/programmers/month_4/Program_3.java
@@ -1,81 +1,55 @@
 package codingTest.programmers.month_4;
 
-import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Program_3 {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+    public static void main(String[] args){
         Program_3 p3 = new Program_3();
 
         long t = p3.solution(new int[] {-5,0,2,1,2},new int[][] {{0,1},{3,4},{2,3},{0,3}});
         System.out.println(t);
     }
 
-    public static Tree[] graph;
+    public static ArrayList<Integer>[] graph;
+    public static boolean[] isVisited;
+    public static int[] w;
+    public static long count;
+    public static long[] check;
     public long solution(int[] a, int[][] edges) {
-        long answer = 0;
 
-        long temp = 0;
-        for (int i = 0; i < a.length; i++)
-            temp += a[i];
-        if (temp != 0)
-            return -1;
-
-        graph = new Tree[a.length];
+        graph = new ArrayList[a.length];
+        isVisited = new boolean[a.length];
+        w = a;
+        count = 0;
+        check = new long[a.length];
         for (int i = 0; i < graph.length; i++)
-            graph[i] = new Tree(a[i]);
+            graph[i] = new ArrayList();
 
         for (int i = 0; i < edges.length; i++) {
-            graph[edges[i][0]].egde.add(edges[i][1]);
-            graph[edges[i][1]].egde.add(edges[i][0]);
+            graph[edges[i][0]].add(edges[i][1]);
+            graph[edges[i][1]].add(edges[i][0]);
         }
 
-        for (int i = 0; i < graph.length; i++) {
-            if (!graph[i].egde.isEmpty() && !graph[i].isVisited) {
-                answer += zeroSum(i, graph[i].egde.get(0));
-                graph[graph[i].egde.get(0)].egde.remove((Integer)i);
-                graph[i].egde.remove(0);
-            }
-        }
+        isVisited[0] = true;
+        dfs(0);
 
-        return answer;
-    }
-
-    public long zeroSum(int index, int target) {
-        long count = 0;
-        while(graph[index].weight != 0) {
-            if (graph[index].weight < 0) {
-                graph[index].weight++;
-                graph[target].weight--;
-            }
-            else {
-                graph[index].weight--;
-                graph[target].weight++;
-            }
-            count++;
-        }
-        graph[index].isVisited = true;
         return count;
     }
+    public void dfs(int node) {
+        for (int i = 0; i < graph[node].size(); i++) {
+            if (!isVisited[graph[node].get(i)]) {
+                isVisited[graph[node].get(i)] = true;
+                dfs(graph[node].get(i));
+                check[node] += check[graph[node].get(i)];
+                count += Math.abs(check[graph[node].get(i)]);
+            }
+        }
+        check[node] += w[node]; //check 배열에 가중치 추가
+        if (node == 0 && check[node] != 0)
+            count = -1;
 
-
-}
-
-class Tree {
-    public ArrayList<Integer> egde;
-    public int weight;
-    public boolean isVisited;
-
-    public Tree(int weight) {
-        this.weight = weight;
-        this.egde = new ArrayList<>();
-        this.isVisited = false;
     }
 
 }
+
